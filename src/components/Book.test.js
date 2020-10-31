9021d49aaf51b1070c0ea3586b2344493717cb5b
Zyta <zyta.slowianska@gmail.com>
2020-10-31 14:22:21 +0100

    chore: Repair linter errors

--- src/components/Book.test.js
+++ src/components/Book.test.js
@@ -6,9 +6,9 @@ import { staticBooksMock } from './__mock__/mockData';
 describe('Book', () => {
   it('should render book representation', () => {
     const bookComponent = render(<Book
-                                    bookCoverURL = {staticBooksMock[0].bookCoverURL}
-                                    bookTitle = {staticBooksMock[0].bookTitle}
-                                    bookAuthors = {staticBooksMock[0].bookAuthors} />);
+      bookCoverURL = {staticBooksMock[0].bookCoverURL}
+      bookTitle = {staticBooksMock[0].bookTitle}
+      bookAuthors = {staticBooksMock[0].bookAuthors} />);
     expect(bookComponent).toMatchSnapshot();
   });
 });
