95bf6b7d59a52bef5e28bbcca6553e848775fb88
Zyta <zyta.slowianska@gmail.com>
2020-11-07 18:57:12 +0100

    test: Linting changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, getByText, render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import App from './App';
 
@@ -10,17 +10,17 @@ describe('App', () => {
     });
 
     it('should open Search page when plus button is clicked', () => {
-      const appComponent = render(<App />);
-      expect(screen.queryByText(/MyReads/i)).toBeVisible();
-      fireEvent.click(screen.queryByText(/Add a book/i));
-      expect(appComponent).toMatchSnapshot();
+        const appComponent = render(<App />);
+        expect(screen.queryByText(/MyReads/i)).toBeVisible();
+        fireEvent.click(screen.queryByText(/Add a book/i));
+        expect(appComponent).toMatchSnapshot();
     });
 
     it('should go back to the reader\'s Bookshelf when go back button is clicked', () => {
-      const appComponent = render(<App />);
-      fireEvent.click(screen.queryByText(/Add a book/i));
-      expect(appComponent).toMatchSnapshot();
-      fireEvent.click(screen.queryByText(/Close/i));
-      expect(appComponent).toMatchSnapshot();
-    })
+        const appComponent = render(<App />);
+        fireEvent.click(screen.queryByText(/Add a book/i));
+        expect(appComponent).toMatchSnapshot();
+        fireEvent.click(screen.queryByText(/Close/i));
+        expect(appComponent).toMatchSnapshot();
+    });
 });
--- src/components/SearchPage.test.js
+++ src/components/SearchPage.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, getByPlaceholderText, render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import SearchPage from './SearchPage';
 
 describe('SearchPage', () => {
@@ -10,15 +10,15 @@ describe('SearchPage', () => {
     });
 
     it('should update component state when a text is input', () => {
-      const onReturnClickMock = jest.fn();
-      const inputMock = 'Lahiri';
-      const searchPageComponent = render(<SearchPage onReturnClick = { onReturnClickMock } />);
-      //const inputNode = screen.getByPlaceohlderText('Search by title or author');
-      fireEvent.change(searchPageComponent.getByPlaceholderText(/Search by title or author/i), {
-        target: {
-          value: inputMock
-        }
-      });
-      expect(searchPageComponent).toMatchSnapshot();
+        const onReturnClickMock = jest.fn();
+        const inputMock = 'Lahiri';
+        const searchPageComponent = render(<SearchPage onReturnClick = { onReturnClickMock } />);
+        //const inputNode = screen.getByPlaceohlderText('Search by title or author');
+        fireEvent.change(searchPageComponent.getByPlaceholderText(/Search by title or author/i), {
+            target: {
+                value: inputMock
+            }
+        });
+        expect(searchPageComponent).toMatchSnapshot();
     });
 });
