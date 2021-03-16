4677bfa6233f0b4bd01b1446966c1acb2a1bb44f
Zyta <zyta.slowianska@gmail.com>
2021-03-16 14:36:12 +0100

    SearchPage.state is undefined

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import * as BooksAPI from '../BooksAPI';
+import Book from './Book';
+
 class SearchPage extends Component {
   state = {
-      query: ''
+      query: '',
+      queryResults: [],
   }
 
 updateQuery = (query) => {
     this.setState(() => ({
         query: query
     }));
+}
+
+static getDerivedStateFromProps = () => {
+    if(this.state.query) {
+        // ask API and update queryResults
+        BooksAPI.search(this.state.query).
+            then((queryResults) => {
+                this.setState(() => {
+                    queryResults;
+                });
+            });
+        return true;
+    } else return false;
 }
 
 render() {
@@ -28,7 +45,16 @@ render() {
             </div>
             <div className="search-books-results">
                 <ol className="books-grid"></ol>
+                {this.state.queryResults.map((book) => (
+                    <li key={book.id}>
+                        <Book
+                            book={book}
+                            //onBookShelfChange={props.onBookShelfChange}
+                        />
+                    </li>
+                ))}
             </div>
+            <p>{this.state.query}</p>
         </div>
     );
 }
